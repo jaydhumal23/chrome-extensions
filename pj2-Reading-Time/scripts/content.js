649a41adaf80153302f9d7d498236e3b8eb93207
Jay Dhumal <jaydhumal27@gmail.com>
2026-01-21 01:28:41 +0530

    finished reading-time extension

--- pj2-Reading-Time/scripts/content.js
+++ pj2-Reading-Time/scripts/content.js
@@ -8,5 +8,26 @@ function renderReadingTime(article) {
     const wordMatchRegExp = /[^\s]+/g;
     const words = text.matchAll(wordMatchRegExp);
     const wordCount = [...words].length;
-
+    const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("p");
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`;
+    const heading = article.querySelector("h1");
+    const date = article.querySelector("time")?.parentNode;
+    (date ?? heading).insertAdjacentElement("afterend", badge)
 }
+renderReadingTime(document.querySelector("article"))
+
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+            if (node instanceof Element && node.tagName === 'ARTICLE') {
+                renderReadingTime(node)
+            }
+        }
+    }
+
+});
+observer.observe(document.querySelector('devsite-content'), {
+    childList: true
+})
